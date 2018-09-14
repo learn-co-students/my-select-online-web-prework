1e317d1e8b6a14ec4834a533ace719462692f95d
Carolyn Whelpley <carolynwhelpley@Jeffs-MacBook-Pro-3.local>
2018-09-13 20:53:05 -0400

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
- # your code here!
+  new_collection = []
+  index = 0
+  while index < collection.length
+    item = collection[index]
+    should_include = yield(item)
+    if should_include 
+      new_collection.push(item)
+    end
+    index = index +1
+  end
+  new_collection
 end
