4a8b0df58ac3524c689b3a3e3ba6415f0ca695bb
Kevin J Gomez <kjgmez1@gmail.com>
2018-09-28 21:08:28 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(collection)
   i=0
   ar=[]
   while i < collection.length
-    ar << yield(collection[i])
+    ar << yield(collection[i]) if collection[i] == true
     i += 1
   end
   ar
