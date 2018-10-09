23c3ab9894156f68e0364fb8f4f04a1179039b51
Brandon Teich <brandonteich123@gmail.com>
2018-10-09 23:13:16 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,8 @@ def my_select(collection)
 x = 0
 newarrray =[]
 while x < collection.length
-  if yield(collection[x]) == true
+  if yield(collection[x]) == true 
+    if(collection.length == 0)
     newarray.push(collection[x])
   end
   x = x + 1
