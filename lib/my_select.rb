f6de6f51fa958212bd0b5b6ada58af8be8d2493f
Eric Mayfield <eric@visiblycreative.com>
2018-10-18 22:03:32 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,19 @@
 def my_select(collection)
- # your code here!
+ count = 0
+ array = []
+ if collection.length > 0
+   while count < collection.length
+     if(yield(collection[count]) == true)
+       array << collection[count]
+     end
+     count += 1
+   end
+   array
+ end
+end
+
+collection = [1,2,3,4,5]
+
+my_select(collection) do |item|
+  item.even?
 end
