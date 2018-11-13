6c4dd6fa02fcb11b469388ad06d145da3d7c478d
Janita Madramootoo <jaymadr@gmail.com>
2018-11-13 18:29:16 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,26 +1,17 @@
-# numbers = [1, 2, 3, 4, 5]
-#
-# def even_numbers(array)
-#   matches = []
-#   array.each do |n|
-#    matches << n if n.even?
-#   end
-#   puts matches
-# end
-#
-# even_numbers(numbers)
-
 def my_select(collection)
   if block_given?
-   i = 1
-   new_array = []
+    index = 0
+    new_array = []
 
-   while i < collection.length
-     yield(collection[i])
-     new_array << i if i.even?
-     i += 1
-   end
-  p new_array
+    while index < collection.length
+      yield(collection[index])
+        for n in collection
+          new_array << n if n.even?
+          index += 1
+        end
+    return new_array
+  end
+
   else
     "This block should not run!"
   end
