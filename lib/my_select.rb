0382dfdb0f29f6ab84dd55c73f9872c7fc66e27f
Wendi Chen <epanistamai.proles@gmail.com>
2018-11-28 03:33:37 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
 def my_select(collection)
- # your code here!
+  i = 0
+  new_col = []
+    while i < collection.length
+      new_num = yield collection[i]
+        if new_num == true
+          new_col << collection[i]
+        end
+      i += 1
+  end
+  new_col
+  
+
 end
