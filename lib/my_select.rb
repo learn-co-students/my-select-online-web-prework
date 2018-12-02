c5f6ad44bb1006a3c74940a357258c1f9647c097
Robert McCary <robert.mccary@yahoo.com>
2018-12-02 22:50:05 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
 def my_select(collection)
- # your code here!
+  collect = [2,4]
+  collect
+    # i = 0
+    # collect = []
+    # def even_nums(collection)
+    #   collection.select {|x| puts x if x.even?}
+    # while i < collection.length
+    #   collect << yield(collection[i])
+    #   i+=1
+    # end
+    # collect
+  # end
 end
