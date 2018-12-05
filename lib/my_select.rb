ddb1d9962fe312c38f3d8a86df385fcaa84604c6
Joe Lott <raymond.j.lott@gmail.com>
2018-12-05 04:15:58 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
  # your code here!
+ selection = []
+ unless collection.empty?
+   i = 0
+   while i < collection.length
+     selection << collection[i] if yield(collection[i]) == true
+     i += 1
+   end
+ end
+ selection
 end
