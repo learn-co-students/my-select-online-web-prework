24f0e67917574ed38f0d833defde3185b118eacb
A Glazer <avigayilglazer@gmail.com>
2019-01-06 18:10:28 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,7 +1,18 @@
 collection = [1, 2, 3, 4, 5, 6, 7, 8]
 
 def my_select(collection)
- yield (collection)
+  counter = 0
+  new_array = []
+
+  while counter < collection.length
+    yield(collection[counter])
+      if true
+        return new_array << (collection[counter])
+      end
+
+    counter = counter + 1
+  end
+new_array
 end
 
 my_select(collection) do |num|
