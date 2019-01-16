ebe461149bf3624c536c60e1eb9d7fa1b83bfc01
Derek Cerretani <derekcerretani@gmail.com>
2019-01-16 19:12:08 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,5 +1,30 @@
+require 'pry'
+
+
 def my_select(collection)
 
   collection.select { |num| num.even? }
 
 end
+
+
+=begin
+nums = [1, 2, 3, 4, 5]
+
+def my_select(collection)
+  if block_given?
+    i = 0
+    selects = []
+      while i < collection.length
+        selects << yield(collection[i])
+        i += 1
+      end
+    selects
+  else
+    puts "No block given."
+  end
+end
+
+my_select(nums) { |i| i.even? }
+
+=end
