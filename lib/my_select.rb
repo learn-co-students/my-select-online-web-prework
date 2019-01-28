a786d30445174785274d088e8d70b1f311534542
Lu Daley <daleyluis@gmail.com>
2019-01-28 01:48:55 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,25 +2,17 @@ def my_select(collection)
   counter = 0 
   new_array = []
   while counter < collection.length
-    counter += 1 
-    if collection.even? == true
-      new_array << yield(collection.even) 
+    if yield(collection[counter])
+      new_array << collection[counter] 
     end
+    counter += 1 
   end
   new_array
 end 
 
+#collection[counter]  aka array[index]= pull elements from array
+#yield --> num.even on collection counter ---> representing block of code in - each element in the array/spec file.
+
+
 #how do i select something that is true/based on boolean parameters?
 #if blank is true then puts blank.true
-
-=begin
-i = 0
-while i < array.size
-  if yield(array[i])
-     # shovel here if conditional fulfilled
-  end
-  #increment i
-end
-# etc.
-
-=end
