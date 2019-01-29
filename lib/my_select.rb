ef2cc5612628d0c8b04302ed643181a11221106c
Christoph Gruener <christophgruener@me.com>
2019-01-29 23:25:15 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
 def my_select(collection)
- # your code here!
+ i = 0 
+ selectors = []
+ while i < collection.length 
+  if yield collection[i] 
+    selectors.push(collection[i])
+  end
+ i += 1 
+end 
+selectors
 end
