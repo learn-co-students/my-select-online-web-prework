7188fe2bc782a0521a204c33997f66548cd4e1c8
Kyle Spencer <spencerk2@vcu.edu>
2019-02-01 01:59:57 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,32 @@
-def my_select(collection)
- # your code here!
+def my_select(array)
+ 
+ 
+ 
+
+collected_array = []
+select_array = []
+  
+  i = 0
+  while i < array.length
+    c = yield array[i]
+    collected_array << c 
+    i = i + 1
+  end
+  
+  i=0
+  collected_array
+  while i < collected_array.length
+    if collected_array[i] == true
+     select_array << array[i]
+    end
+  i = i + 1
+  end
+  select_array
+
+ 
+ 
+ 
+ 
+ 
+ 
 end
