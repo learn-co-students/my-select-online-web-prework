7674a8ba9f0acc1b707c8ce3d0603f43ac242d09
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-02-08 01:07:33 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,15 @@
+require 'pry'
+# What is the return value of yielding a character to a block?
+# How can you ensure your .select method can respond appropriately to being passed an argument of an empty collection?
+
 def my_select(collection)
- # your code here!
+  evens = []
+  i = 0
+  while i < collection.length
+    if yield(collection[i])
+      evens << collection[i]
+    end
+    i += 1
+  end
+  return evens
 end
