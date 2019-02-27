f2a2a4c3e257738484de9dcd7e6744c8ddfcf36f
David Stewart <davidstewart@gmail.com>
2019-02-27 17:05:30 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,15 @@
+
+
+require 'pry'
+test = [1,2,3,4]
 def my_select(collection)
- # your code here!
+  i = 0
+  output = []
+  while i < collection.length
+  (yield(collection[i])) ? (output << collection[i]) : (nil) 
+  puts output[i]
+  i = i + 1
+  end
+  return output
 end
+#binding.pry
