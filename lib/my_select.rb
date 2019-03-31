bf1625ba8ab539da2adfd1c10e42337385bac69d
Jennifer Dirmeyer <dirmeyer@gmail.com>
2019-03-31 18:32:10 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
-def my_select(collection)
- # your code here!
-end
+def my_select(array)
+  i = 0 
+  selection = []
+  while 
+    i < array.length 
+    result = yield (array[i])
+    if result == true
+    selection<< array[i]
+    end
+    i = i + 1 
+  end  
+  selection
+end  
+   
