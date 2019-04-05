bda493443d8f63d7b64ef8bbc458f855aee9a4bd
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-04-05 23:25:54 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,18 @@
 def my_select(collection)
  # your code here!
+new_collection = []
+  i = 0 
+  while i < collection.length
+    if yield(collection[i]) #if true
+      new_collection.push(collection[i])
+    end
+  i += 1 
+  end 
+  new_collection
+end
+
+
+
+my_select([1, 2, 3, 4, 5]) do |item|
+  item.even?
 end
