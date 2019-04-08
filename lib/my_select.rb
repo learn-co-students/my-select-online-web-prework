59a8d8f92b2254818e70613a24791c5ced87e6b1
Cody Alvarez <jedi.alvarez805@gmail.com>
2019-04-08 12:58:36 -0400

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,21 @@
+## Person 1
 def my_select(collection)
- # your code here!
+    new_arry = []
+    collection.each do |element|
+        if yield element 
+            new_arry << element
+        end 
+    end
+    return new_arry
 end
+
+# ## Person 2
+# col = ['apples', 'oranges', 'pears']
+# col2 = [1,2,3,4,5,6]
+# my_select(col) do |item|
+#   item[0].downcase == "a"
+# end
+
+# my_select(col2) do |num|
+#     num % 3 == 0
+# end
