65a8bad546ce7c01eb40932af5433652a2379a90
Caleb Einarr <earthctzn1@gmail.com>
2019-04-08 20:35:47 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,18 @@
 def my_select(collection)
- # your code here!
+  new_a = []
+  count = 0
+  while count < collection.length
+    value = collection[count]
+    truthiness = yield collection[count]
+    if truthiness == true
+      new_a << value
+  end
+    count+=1
+  end
+  new_a
+end
+
+new_a = []
+my_select([1, 2, 3, 4, 5]) do |num|
+   num.even?
 end
