46f61f72e136ce908e9614eae2326f77d8806923
Kish Siva <rodes88@msn.com>
2019-04-10 11:42:34 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,16 @@
 def my_select(collection)
- # your code here!
+ 
+ arr=[]
+ counter = 0 
+ other_counter = 0
+ while counter < collection.length
+  if (yield(collection[counter]) == true)
+    arr[other_counter] = collection[counter]
+    other_counter += 1 
+  end
+ counter+=1
+ end 
+ arr
+  
 end
+
