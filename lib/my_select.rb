1d6040ff23e5dfdda3d68cfb8c33821d97dc8e6b
Jason McGloin <jason.mcgloin@gmail.com>
2019-04-26 15:08:41 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
- # your code here!
+  if collection.empty? then return [] end
+  i = 0
+  newArray = []
+  while i < collection.count do
+    if yield(collection[i]) then newArray.push(collection[i]) end
+    i += 1
+  end
+  newArray
 end
+
+my_select([1,2,3,4,5]) { |item| item.even? }
