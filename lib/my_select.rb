5aa7863cdd4d0f0983887c357c6792313711f27d
Lindsey Wells <lindsey.wells84@gmail.com>
2019-05-02 11:20:39 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,20 @@
-def my_select(collection)
- # your code here!
+=begin def my_select(array)
+ i = 0
+array2 = []
+while i < array.length
+  array2 << array[i] if yield(array[i])
+  i+=1
 end
+array2
+end
+end
+=end
+
+# your code here!
+	def my_select(nums)
+  nums.select do |num|
+  	num.even?
+  end
+  end
+
+
