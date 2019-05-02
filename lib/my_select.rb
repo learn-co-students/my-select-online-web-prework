9dde6d9562db42d7622760e999ff9f2d1861f313
Lindsey Wells <lindsey.wells84@gmail.com>
2019-05-02 11:28:05 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,20 +1,14 @@
-=begin def my_select(array)
+def my_select(array)
  i = 0
 array2 = []
 while i < array.length
-  array2 << array[i] if yield(array[i])
+  if yield(array[i]) == true
+    array2 << array[i] 
+  end
   i+=1
 end
 array2
 end
-end
-=end
 
-# your code here!
-	def my_select(nums)
-  nums.select do |num|
-  	num.even?
-  end
-  end
 
 
