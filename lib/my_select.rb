1e50ba142506b94af352e7d225ce3ca9be0d1e6e
communal-tutorial-3267 <jrodden1@gmail.com>
2019-05-30 21:38:56 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
 def my_select(collection)
- # your code here!
+  if block_given?
+    i = 0
+    new_collection = []
+    while i < collection.length
+      yielded_value = yield collection[i]
+      if yielded_value == true
+        new_collection << collection[i]
+      end
+      i = i + 1
+    end
+  end
+  new_collection
 end
