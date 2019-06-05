64a0f96834cc160db14ebd0f47134ab30a342777
kpobrien4 <48386395+kpobrien4@users.noreply.github.com>
2019-06-05 12:33:07 -0400

    Update my_select.rb
    Created the select method using the while and yield methods.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,15 @@
-def my_select(collection)
- # your code here!
+collection = [1, 2, 3, 4, 5]
+
+def my_select(array)
+  i = 0
+  select = []
+  while i < array.length
+    if yield(array[i])
+      select << array[i]
+    end
+    i+=1
+  end
+  select
 end
+
+my_select(collection) { | num | num.even? }
