6641c38f04d6f39bda6539339b88168483e8fe1c
witty-banner-1950 <asimsali95@gmail.com>
2019-06-24 19:56:28 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,16 @@
 def my_select(collection)
  # your code here!
+ i = 0
+ new_collection = []
+ while i < collection.size
+   if yield (collection[i]) == true
+     new_collection << collection[i]
+   else
+
+   end
+   i += 1
+ end
+ new_collection
 end
+
+my_select(collection {|num| num.even?})
